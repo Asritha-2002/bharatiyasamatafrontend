ec2e2e14e67b43b4fb964c7b256f06f4a1c6bf36
Saladi Asritha Devi <saladiasrithadevi@gmail.com>
2026-07-13 14:10:05 +0530

    after adding checkout page

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://bharatiyasamata-theta.vercel.app/api'
 });
 //https://bharatiyasamata-theta.vercel.app
 api.interceptors.request.use((config) => {
